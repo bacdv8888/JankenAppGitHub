07a5e3527f42abfe611e85fd805a3888f8cbad04
bacdv8888 <doanvanbac8888@gmail.com>
2025-11-13 10:22:53 +0900

    update code

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: {
+    // '@tailwindcss/postcss': {}, // Tailwind CSS v4 - sử dụng @tailwindcss/postcss thay vì tailwindcss
+    // autoprefixer: {},
+    '@tailwindcss/postcss': {}, 
+    autoprefixer: {},
+  },
+}
